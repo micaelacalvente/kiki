25b689621fe51463233f5d0a39d5417e31fe3736
micaela <micaelacalvente@gmail.com>
2022-10-17 15:29:53 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .flex .navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
+
+//fa-times es el icono X y active activa el menu de nav
